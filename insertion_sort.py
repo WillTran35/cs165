cdfc0f8416ba5d4923c13cb0aca6ddd730700f7f
William Tran <williamtran807@gmail.com>
2025-04-01 16:01:24 -0700

    first commit

---
+++ insertion_sort.py
@@ -0,0 +1,12 @@
+def insertion_sort():
+
+    x = [1, 6, 7, 8, 9, 10, 2]
+    for i in x:
+        print(i)
+
+    for i in range(len(x)):
+        print(i)
+
+if __name__ == '__main__':
+    insertion_sort()
+
