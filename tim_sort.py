3e9d6a1bb00f72167f5d7edced75b0d1b0bce94f
William Tran <williamtran807@gmail.com>
2025-04-02 17:28:06 -0700

    tim_sort

--- tim_sort.py
+++ tim_sort.py
@@ -17,32 +17,71 @@ def make_runs(arr):
 
     return result
 
-def merge(first, next):
-    pass
+def merge(first, second):
+    first_len = len(first)
+    second_len = len(second)
+    pos1 = 0
+    pos2 = 0
+    result = []
+    while not pos1 >= first_len and not pos2 >= second_len:
+        # print(pos1, pos2)
+        if first[pos1] < second[pos2]:
+            result.append(first[pos1])
+            pos1 += 1
+        elif first[pos1] > second[pos2]:
+            result.append(second[pos2])
+            pos2 += 1
+        else:
+            result.append(first[pos1])
+            result.append(second[pos2])
+            pos1 += 1
+            pos2 += 1
+
+    if pos1 < first_len:
+        result += first[pos1:]
+
+    elif pos2 < second_len:
+        result += second[pos2:]
+
+    return result
+
 
 def tim_sort(arr):
     runs = make_runs(arr)
-    stack = deque()
-    while len(runs) > 0:
+    stack = []
+    while len(runs) != 0:
         run = runs.pop(0)
         stack.append(run)
         height = len(stack)
+        print(f"this is : {len(runs)}" )
         while True:
-            if height >= 3 and len(stack[1]) > len(stack[3]):
-                merge(stack[2], stack[3])
-            elif height >= 2 and len(stack[1]) >= len(stack[2]):
-                merge(stack[1], stack[2])
-            elif height >= 3 and len(stack[1]) + len(stack[2]) >= len(stack[3]):
-                merge(stack[1], stack[2])
-            elif height >= 4 and len(stack[2]) + len(stack[3]) >= len(stack[4]):
-                merge(stack[1], stack[2])
-            else:
-                break
+            if height >= 3 and len(stack[0]) > len(stack[2]):
+                stack[1] = merge(stack[1], stack[2])
+                stack.pop(2)
+            elif height >= 2 and len(stack[0]) >= len(stack[1]):
+                stack[0] = merge(stack[0], stack[1])
+                stack.pop(1)
+            elif height >= 3 and len(stack[0]) + len(stack[1]) >= len(stack[2]):
+                stack[0] = merge(stack[0], stack[1])
+                stack.pop(1)
+            elif height >= 4 and len(stack[1]) + len(stack[2]) >= len(stack[3]):
+                stack[0] = merge(stack[0], stack[1])
+                stack.pop(1)
+
+            print(f"stack: {stack}")
+            break
     while len(stack) != 1:
-        merge(stack[1], stack[2])
+        stack[1] = merge(stack[1], stack[2])
+        stack.pop(2)
+    return stack[0]
 
 
 if __name__ == '__main__':
     x = [4,5,9,3,1,10]
-    print(make_runs(x))
+    # print(make_runs(x))
+    # x = [4,5,9]
+    # y = [1,10]
+    # print(merge(x,y))
+
+    print(tim_sort(x))
 
